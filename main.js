a7edecdc483f289e69a7a7764bf6579b644c7c08
Thiago Andrade <01tpaa.br@gmail.com>
2021-09-06 16:30:50 -0300

    operators

--- main.js
+++ main.js
@@ -1,36 +1,88 @@
-var viewX = view._viewSize._width
-var viewY = view._viewSize._height
-
-//To know where to realocate points
-var oldViewCenter = view.center
-
-var resolution = 50
-
-var xDist = viewX/resolution
-var yDist = viewY/resolution
-
-var circlesList = []
-path = new Path()
-
-for (var i = 0; i <= resolution; i++) {
-	for(var j = 0; j <= resolution; j++){
-		var newCircle = new Path.Circle(new Point(i * xDist, j * yDist), 0.8)
-		newCircle.strokeColor = 'black';
-
-		circlesList.push(newCircle)
-	}
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var viewX = view._viewSize._width;
+var viewY = view._viewSize._height;
 
-function onResize(event) {
-	var diference = view.center - oldViewCenter
-	oldViewCenter = view.center
+//To know where to realocate points
+var oldViewCenter = view.center;
 
-	// Whenever the window is resized, realocate grid:
-	for (var i = 0;  i < circlesList.length; i++) {
-		circlesList[i].position += diference
+var resolution = 30;
+
+var xDist = viewX/resolution;
+var yDist = viewY/resolution;
+
+var centersList = [];
+path = new Path();
+path.strokeColor = 'red';
+
+
+for (var i = 0; i <= resolution; i++) {
+	var newColumn = []
+	for(var j = 0; j <= resolution; j++){
+		var newPoint = new Point(i * xDist, j * yDist);
+		newColumn.push(newPoint);
+		
 	}
 
-	viewX = view._viewSize._width
-	viewX = view._viewSize._width
+	centersList.push(newColumn);
+}
+
+//Operators
+var continueCirclePath = new Path.Circle(new Point(0,0), 3)
+continueCirclePath.fillColor = 'white';
+
+var stopCirclePath = new Path.Circle(new Point(0,0), 3)
+stopCirclePath.fillColor = 'red';
+
+var changeDirCirclePath = new Path.Circle(new Point(0,0), 3)
+changeDirCirclePath.fillColor = 'blue';
+
+var continueCircle = new SymbolDefinition(continueCirclePath);
+var stopCircle = new SymbolDefinition(stopCirclePath);
+var changeDirCircle = new SymbolDefinition(changeDirCirclePath);
+
+function onResize(event) {
+	var diff = view.center - oldViewCenter;
+	oldViewCenter = view.center;
+
+	viewX = view._viewSize._width;
+	viewX = view._viewSize._width;
+}
+
+//For animation
+var lastTick = 0
+
+function onFrame(event) {
+
+}
+
+var lastKey = 'q';
+var keysList = ['q', 'w', 'e'];
+var symbolsList = [continueCircle, stopCircle, changeDirCircle];
+
+function onMouseDown(event) {
+	//Finding out where clicked inside grid
+	var clickedX = Math.round(event.point.x / xDist)
+	var clickedY = Math.round(event.point.y / yDist)
+	
+	if(clickedX > resolution) {
+		clickedX = resolution
+	}
+
+	if(clickedY > resolution) {
+		clickedY = resolution
+	}
+
+	var instance = symbolsList[keysList.indexOf(lastKey)].place();
+	instance.position = centersList[clickedX][clickedY];
+}
+
+function onKeyDown(event) {
+	if(event.key == 'q' || event.key == 'w' || event.key == 'e'){
+		lastKey = event.key;
+	}
 }
