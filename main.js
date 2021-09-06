a7d7aa246a18d0dedbf1362b974fc274092e60ee
Thiago Andrade <01tpaa.br@gmail.com>
2021-09-06 15:11:38 -0300

    grid

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-// Create a circle shaped path with its center at the center
-// of the view and a radius of 30:
-var path = new Path.Circle({
-	center: view.center,
-	radius: 245,
-	strokeColor: 'pink'
-});
+var viewX = view._viewSize._width
+var viewY = view._viewSize._height
+
+//To know where to realocate points
+var oldViewCenter = view.center
+
+var resolution = 50
+
+var xDist = viewX/resolution
+var yDist = viewY/resolution
+
+var circlesList = []
+path = new Path()
+
+for (var i = 0; i <= resolution; i++) {
+	for(var j = 0; j <= resolution; j++){
+		var newCircle = new Path.Circle(new Point(i * xDist, j * yDist), 0.8)
+		newCircle.strokeColor = 'black';
+
+		circlesList.push(newCircle)
+	}
+}
+
 
 function onResize(event) {
-	// Whenever the window is resized, recenter the path:
-	path.position = view.center;
+	var diference = view.center - oldViewCenter
+	oldViewCenter = view.center
+
+	// Whenever the window is resized, realocate grid:
+	for (var i = 0;  i < circlesList.length; i++) {
+		circlesList[i].position += diference
+	}
+
+	viewX = view._viewSize._width
+	viewX = view._viewSize._width
 }
