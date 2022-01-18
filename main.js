d322227e188f9f07447a26a574b7b2fd8a7e95e0
Thiago Andrade <01tpaa.br@gmail.com>
2022-01-18 15:02:29 -0300

    t

--- main.js
+++ main.js
@@ -16,7 +16,15 @@ var xDist = viewX/resolution;
 var yDist = viewY/resolution;
 
 
-//Operators
+var basicRectanglePath = new Path.Rectangle(new Point(0,0), 19);
+basicRectanglePath.fillColor = 'white';
+
+var celulaVivaPath = new Path.Rectangle(new Point(0,0), 19);
+celulaVivaPath.fillColor = 'green';
+
+var zumbiPath = new Path.Rectangle(new Point(0,0), 19);
+zumbiPath.fillColor = 'red';
+
 var basicCirclePath = new Path.Circle(new Point(0,0), 3)
 basicCirclePath.fillColor = 'grey';
 
@@ -29,6 +37,9 @@ stopCirclePath.fillColor = 'red';
 var changeDirCirclePath = new Path.Circle(new Point(0,0), 3)
 changeDirCirclePath.fillColor = 'blue';
 
+var basicRect = new SymbolDefinition(basicRectanglePath);
+var celulaViva = new SymbolDefinition(celulaVivaPath);
+var zumbi = new SymbolDefinition(zumbiPath);
 var basicCircle = new SymbolDefinition(basicCirclePath);
 var continueCircle = new SymbolDefinition(continueCirclePath);
 var stopCircle = new SymbolDefinition(stopCirclePath);
@@ -36,22 +47,28 @@ var changeDirCircle = new SymbolDefinition(changeDirCirclePath);
 
 var lastKey = 'q';
 var keysList = ['q', 'w', 'e', 'r'];
-var symbolsList = [basicCircle, continueCircle, stopCircle, changeDirCircle];
+var symbolsList = [basicRect, celulaViva, zumbi, basicCircle, continueCircle, stopCircle, changeDirCircle];
 
 
 //Grid
 var centersList = [];
 var gridList = [];
+var n = 20
 
-for (var i = 0; i <= resolution; i++) {
+for (var i = 1; i <= n; i++) {
 	var newColumn = []
 	var newGridColumn = [];
-	for(var j = 0; j <= resolution; j++){
-		var newPoint = new Point(i * xDist, j * yDist);
+	for(var j = 1; j <= n; j++){
+		var newPoint = new Point(i * 20, j * 20);
+
 		newColumn.push(newPoint);
 		
-		var instance = symbolsList[0].place();
+		var instance = symbolsList[1].place();
+		var instance2 = symbolsList[0].place();
+
 		instance.position = newPoint;
+		instance2.position = newPoint;
+
 		newGridColumn.push(instance);
 		
 	}
@@ -77,24 +94,9 @@ function onFrame(event) {
 
 
 function onMouseDown(event) {
-	//Finding out where clicked inside grid
-	var clickedX = Math.round(event.point.x / xDist)
-	var clickedY = Math.round(event.point.y / yDist)
 	
-	if(clickedX > resolution) {
-		clickedX = resolution
-	}
-
-	if(clickedY > resolution) {
-		clickedY = resolution
-	}
-
-	var instance = symbolsList[keysList.indexOf(lastKey)].place();
-	instance.position = centersList[clickedX][clickedY];
 }
 
 function onKeyDown(event) {
-	if(event.key == 'q' || event.key == 'w' || event.key == 'e' || event.key == 'r'){
-		lastKey = event.key;
-	}
+	
 }
